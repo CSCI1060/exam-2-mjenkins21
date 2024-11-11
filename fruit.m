73611be2fe0ad864ee66287b3f9cd18a4cbf276e
mjenkins21 <marissa.jenkins@slu.edu>
2024-11-11 08:48:26 -0600

    Update fruit.m

--- fruit.m
+++ fruit.m
@@ -1,7 +1,14 @@
-function [] = fruit( N )
-% This function prints for values from 1 to N:
-% “apple” if the value is divisible by 2
-% “banana” if the value is divisible by 5
-% The number itself if neither is true
-% “applebanana” if both are true
+function y = fruit(N)
+for z = 1:N
+    if rem(z,2) == 0 && rem(z,5) == 0 
+        fprintf('applebanana\n', z)
+    elseif rem(z,5) == 0 
+        fprintf('banana\n', z)
+    elseif rem(z,2) == 0
+        fprintf('apple\n', z)
+    else 
+        fprintf('%d\n', z)
+
+    end
+end
 
